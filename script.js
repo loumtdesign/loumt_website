08e6872a4fabc0459ec24d873ca5e0a1dc571e13
loumt <lucymorristhomas@gmail.com>
2019-11-17 12:36:58 +0000

    Landing Page

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function mouseOver() {
+  document.getElementById("secondarylogo").src ="images/primary_logo.png";
+}
+
+function mouseOut() {
+  document.getElementById("secondarylogo").src = "images/secondary_logo.png";
+}
